95c1def1dbf171e0d578e1158cc1e44f82ec65c4
Seven Tsai <mvpscottjon@hotmail.com>
2017-07-30 04:23:18 +0800

    20170730v1 1.做完申請入團及審核

---
+++ together/openGroupTbViCell.swift
@@ -0,0 +1,24 @@
+//
+//  openGroupTbViCell.swift
+//  together
+//
+//  Created by Seven Tsai on 2017/7/29.
+//  Copyright © 2017年 Seven Tsai. All rights reserved.
+//
+
+import UIKit
+
+class openGroupTbViCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
