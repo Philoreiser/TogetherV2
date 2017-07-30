63018ba8688239974a250c59bcece4b83d86713d
Seven Tsai <mvpscottjon@hotmail.com>
2017-07-31 01:56:06 +0800

    20170731v1 1.做完揪團管理

---
+++ together/applyGroupTBVCell.swift
@@ -0,0 +1,24 @@
+//
+//  applyGroupTBVCell.swift
+//  together
+//
+//  Created by Seven Tsai on 2017/7/31.
+//  Copyright © 2017年 Seven Tsai. All rights reserved.
+//
+
+import UIKit
+
+class applyGroupTBVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
